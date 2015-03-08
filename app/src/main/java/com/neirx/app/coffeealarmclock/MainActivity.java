0ecd1403d87f3e0f0f85ff82287f83f8914edc85
neritr <neritr@gmail.com>
2015-03-09 01:56:25 +0200

    Add background and top text

--- app/src/main/java/com/neirx/app/coffeealarmclock/MainActivity.java
+++ app/src/main/java/com/neirx/app/coffeealarmclock/MainActivity.java
@@ -1,17 +1,48 @@
 package com.neirx.app.coffeealarmclock;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Calendar;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomDigitalClock dc = (CustomDigitalClock) findViewById(R.id.dgClock);
+
+        Calendar calendar = Calendar.getInstance();
+        int today = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (today) {
+            case Calendar.MONDAY:
+                findViewById(R.id.tvMon).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.TUESDAY:
+                findViewById(R.id.tvTue).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.WEDNESDAY:
+                findViewById(R.id.tvWed).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.THURSDAY:
+                findViewById(R.id.tvThu).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.FRIDAY:
+                findViewById(R.id.tvFri).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.SATURDAY:
+                findViewById(R.id.tvSat).setBackgroundResource(R.drawable.line);
+                break;
+            case Calendar.SUNDAY:
+                findViewById(R.id.tvSun).setBackgroundResource(R.drawable.line);
+                break;
+            default:
+                break;
+        }
     }
 
 
